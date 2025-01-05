70c7b5d9aaba8c0d8e65153f80ad1fb40c2b1433
Renan da S. Silvaira <silveira.s.renan@gmail.com>
2025-01-05 18:32:06 -0300

    Create Pedido, falta controller

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -0,0 +1,18 @@
+package br.com.microservices.orchestrated.orderservice.core.service;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public Event saveEvent(Event event) {
+       return eventRepository.save(event);
+    }
+
+}
